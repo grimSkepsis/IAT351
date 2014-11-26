539f5db31bd64e12163cf0b061484273d94914f2
Michael Hofer <mhofer180993@gmail.com>
2014-11-26 01:06:57 -0800

    Added Filtering Functionality
    Requires db update

--- iat351/ajax/create_character_circles.php
+++ iat351/ajax/create_character_circles.php
@@ -15,6 +15,20 @@ while($info = $results->fetch_assoc()){
                                           health = "'.$info['health'].'"
                                           guard = "'.$info['guard_gauge'].'"
                                           utility = "'.$info['utility'].'"
+
+                                          half_circles = "'.$info['num_half_circle'].'"
+                                          double_half_circles = "'.$info['num_double_half_circle'].'"
+                                          quarter_circles = "'.$info['num_quarter_circle'].'"
+                                          double_quarter_circles = "'.$info['num_double_quarter_circle'].'"
+                                          dragon_punches = "'.$info['num_dp'].'"
+                                          three_sixties = "'.$info['num_360'].'"
+                                          double_three_sixties = "'.$info['num_double_360'].'"
+                                          pretzel = "'.$info['num_pretzel'].'"
+                                          quarter_half_circles = "'.$info['num_quarter_half_circle'].'"
+                                          half_quarter_circles = "'.$info['num_half_quarter_circle'].'"
+                                          back_charges = "'.$info['num_back_charge'].'"
+                                          down_charges = "'.$info['num_down_charge'].'"
+
                                           ><img src = "img/'.$info['picture_url'].'"></div>';
 }
   $results->free();
